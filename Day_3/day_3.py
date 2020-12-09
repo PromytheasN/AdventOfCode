0e7ed7996d9de4b923c07409e7e703b3101d0336
Alexander Kemos <alforeverandever@gmail.com>
2020-12-09 00:25:16 -0800

    now gives correct result

--- Day_3/day_3.py
+++ Day_3/day_3.py
@@ -10,32 +10,32 @@ def data_extraction():
     line_count = 1
 
     for line in map_input:
-
         line_count += 1
-        tree_lines.append(line.strip() * line_count)
+        tree_lines.append(line.strip() * (line_count))
 
     map_input.close()
 
-    return tree_lines, line_count
+    return tree_lines
 
 
-def count_tree_encounter(tree_lines, line_count):
+def count_tree_encounter(tree_lines):
 
     tree_encounters = 0
-    position = 0
+    position = 3
+    line = 1
 
-    while position <= len(tree_lines[line_count - 2]):
-
-        for line in tree_lines:
-            print(line)
-            if line[position] == "#":
-                tree_encounters += 1
+    # While position < than length of last line - 3 and line within tree_map
+    while position <= len(tree_lines[len(tree_lines) - 3]) and line < len(tree_lines):
+        
+        if tree_lines[line][position] == "#":
+            tree_encounters += 1
+        line += 1
         position += 3
 
     return tree_encounters
 
 
-tree_lines, line_count = data_extraction()
-count = count_tree_encounter(tree_lines, line_count)
+tree_lines = data_extraction()
+count = count_tree_encounter(tree_lines)
 
 print(count)
