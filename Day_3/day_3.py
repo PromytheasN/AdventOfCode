096fb75303caa1e745001c9828fab4f25e89d9c4
Alexander Kemos <alforeverandever@gmail.com>
2020-12-10 11:33:47 -0800

    Flake8 passed

--- Day_3/day_3.py
+++ Day_3/day_3.py
@@ -25,36 +25,26 @@ def count_tree_encounter(tree_lines, right_step, down_step):
     position = right_step
     line = down_step
 
-    # While position < than length of last line - right_step + 1 and line within tree_map length
+    # While position < than length of final line - 1
+    # and line within tree_map length
     while line <= len(tree_lines) - 1:
 
-        print("Current position we are looking and line is: ", position, line)
         if tree_lines[line][position] == "#":
             tree_encounters += 1
-            print("we found one more, total encoutners: ", tree_encounters, "at line", line, "at position", position)
 
         line += down_step
-        #print("new line is: ", line)
         position += right_step
-        print("new position and line that we are going to look is: ", position, "and", line)
     return tree_encounters
 
 
 tree_lines = data_extraction()
-#countr3d1 = count_tree_encounter(tree_lines, 3, 1)
-#countr1d1 = count_tree_encounter(tree_lines, 1, 1)
-#print("R1D1 : ", countr1d1)
+
+# Assiging all slope patterns
+countr3d1 = count_tree_encounter(tree_lines, 3, 1)
+countr1d1 = count_tree_encounter(tree_lines, 1, 1)
 countr5d1 = count_tree_encounter(tree_lines, 5, 1)
-print("R5D1", countr5d1)
-#countr7d1 = count_tree_encounter(tree_lines, 7, 1)
-#print("R7D1", countr7d1)
-#countr1d2 = count_tree_encounter(tree_lines, 1, 2)
-#print("R1D2", countr1d2)
+countr7d1 = count_tree_encounter(tree_lines, 7, 1)
+countr1d2 = count_tree_encounter(tree_lines, 1, 2)
+product = countr1d1 * countr1d2 * countr3d1 * countr5d1 * countr7d1
 
-#print("The number of encounters for right 3 down 1 are: ", countr3d1)
-
-#product = countr1d1 * countr1d2 * countr3d1 * countr5d1 * countr7d1
-
-#print("The product of all encounters is: ", product)
-
-# not 5690521200, 4893848232, 5007658656
+print("The product of the tree encouters of each listed slope is: ", product)
