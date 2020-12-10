0c88dcfe10c180fcb06a277c8c4638a6d867f4cc
Alexander Kemos <alforeverandever@gmail.com>
2020-12-10 08:55:49 -0800

    current line check filter now fixed

--- Day_3/day_3.py
+++ Day_3/day_3.py
@@ -26,13 +26,12 @@ def count_tree_encounter(tree_lines, right_step, down_step):
     line = down_step
 
     # While position < than length of last line - right_step + 1 and line within tree_map length
-    while line <= len(tree_lines) - down_step:
+    while line <= len(tree_lines) - 1:
 
         print("Current position we are looking and line is: ", position, line)
         if tree_lines[line][position] == "#":
             tree_encounters += 1
-            print("we found one more, total: ", tree_encounters)
-            print("Line we found it is: ", line + down_step)
+            print("we found one more, total encoutners: ", tree_encounters, "at line", line, "at position", position)
 
         line += down_step
         #print("new line is: ", line)
@@ -42,20 +41,20 @@ def count_tree_encounter(tree_lines, right_step, down_step):
 
 
 tree_lines = data_extraction()
-countr3d1 = count_tree_encounter(tree_lines, 3, 1)
-countr1d1 = count_tree_encounter(tree_lines, 1, 1)
-print("R1D1 : ", countr1d1)
+#countr3d1 = count_tree_encounter(tree_lines, 3, 1)
+#countr1d1 = count_tree_encounter(tree_lines, 1, 1)
+#print("R1D1 : ", countr1d1)
 countr5d1 = count_tree_encounter(tree_lines, 5, 1)
 print("R5D1", countr5d1)
-countr7d1 = count_tree_encounter(tree_lines, 7, 1)
-print("R7D1", countr7d1)
-countr1d2 = count_tree_encounter(tree_lines, 1, 2)
-print("R1D2", countr1d2)
+#countr7d1 = count_tree_encounter(tree_lines, 7, 1)
+#print("R7D1", countr7d1)
+#countr1d2 = count_tree_encounter(tree_lines, 1, 2)
+#print("R1D2", countr1d2)
 
-print("The number of encounters for right 3 down 1 are: ", countr3d1)
+#print("The number of encounters for right 3 down 1 are: ", countr3d1)
 
-product = countr1d1 * countr1d2 * countr3d1 * countr5d1 * countr7d1
+#product = countr1d1 * countr1d2 * countr3d1 * countr5d1 * countr7d1
 
-print("The product of all encounters is: ", product)
+#print("The product of all encounters is: ", product)
 
 # not 5690521200, 4893848232, 5007658656
